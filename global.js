0f349bc9074e2731043ebe6bac37917c33601374
Kayempea <k.phan905@gmail.com>
2025-01-25 21:38:42 -0800

    Update global.js

--- global.js
+++ global.js
@@ -18,9 +18,21 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+
     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
+
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+    
     nav.append(a);
 }
