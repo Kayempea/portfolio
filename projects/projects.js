62492bfda5b387311a964bb2ddefb6609e7e96b5
Kayempea <k.phan905@gmail.com>
2025-02-01 20:55:28 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -5,5 +5,5 @@ renderProjects(projects, projectsContainer, 'h2');
 
 const projectsTitle = document.querySelector('.projects-title');
 if (projectsTitle) {
-    projectsTitle.textContent = `${projects.length} projects`;
+    projectsTitle.textContent = `${projects.length} Projects`;
 }
