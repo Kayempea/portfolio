4eac965e23429e0d21c28445552caec24425e35c
Kayempea <k.phan905@gmail.com>
2025-02-01 21:40:17 -0800

    Update global.js

--- global.js
+++ global.js
@@ -95,6 +95,15 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     });
 }
 
-export async function fetchGitHubData(username) {
-    return fetchJSON(`https://api.github.com/users/${username}`);
+export async function fetchGithubData(username) {
+    try {
+        const response = await fetch('https://api.github.com/users/${username}');
+        if (!response.ok) {
+            throw new Error('GitHub API request failed: ${response.statusText}');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching GitHub data:', error);
+    }
 }
